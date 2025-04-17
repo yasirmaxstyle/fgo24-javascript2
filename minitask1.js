f0e8e45ce05149bcfbe3183bb8f02fd2b4429f60
yasirmaxstyle <yasirmu77@gmail.com>
2025-04-17 11:36:43 +0700

    add function to determine even or odd number

---
+++ minitask1.js
@@ -0,0 +1,7 @@
+function oddEvenNumber(num) {
+    if (num % 2 === 0) console.log(`nomor ${num} adalah genap`)
+    else console.log(`nomor ${num} adalah ganjil`)
+}
+
+oddEvenNumber(8)
+oddEvenNumber(5)
